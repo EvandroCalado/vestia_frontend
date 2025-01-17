3d9169085943247b4274b208426c42cec2d75b4d
Evandro Calado <evandrocalado07@gmail.com>
2025-01-17 00:08:20 -0300

    feat: Refactor ProductPage to integrate product details and images directly from productDetails object

--- src/app/(app)/products/[slug]/page.tsx
+++ src/app/(app)/products/[slug]/page.tsx
@@ -5,19 +5,33 @@ import {
   ProductTabs,
 } from '@/components/product';
 
-const images = [
-  '/images/slide1.png',
-  '/images/slide2.png',
-  '/images/slide3.png',
-  '/images/slide4.png',
-];
-
 const productDetails = {
   title: 'raw black t-shirt lineup',
+  description: `<p>
+          Elevate your everyday style with our Men's Black T-Shirts, the
+          ultimate wardrobe essential for modern men. Crafted with meticulous
+          attention to detail and designed for comfort, these versatile black
+          tees are a must-have addition to your collection. The classic black
+          color never goes out of style. Whether you're dressing up for a
+          special occasion or keeping it casual, these black t-shirts are the
+          perfect choice, effortlessly complementing any outfit.
+        </p>
+        <ul>
+          <li>Premium Quality</li>
+          <li>Versatile Wardrobe Staple</li>
+          <li>Available in Various Sizes</li>
+          <li>Tailored Fit</li>
+        </ul>`,
   score: 4.2,
   numReviews: 54,
   inStock: true,
   price: 75,
+  images: [
+    '/images/slide1.png',
+    '/images/slide2.png',
+    '/images/slide3.png',
+    '/images/slide4.png',
+  ],
   colors: [
     {
       name: 'blue',
@@ -36,33 +50,17 @@ const productDetails = {
   quantity: 5,
 };
 
-const description = `<p>
-          Elevate your everyday style with our Men's Black T-Shirts, the
-          ultimate wardrobe essential for modern men. Crafted with meticulous
-          attention to detail and designed for comfort, these versatile black
-          tees are a must-have addition to your collection. The classic black
-          color never goes out of style. Whether you're dressing up for a
-          special occasion or keeping it casual, these black t-shirts are the
-          perfect choice, effortlessly complementing any outfit.
-        </p>
-        <ul>
-          <li>Premium Quality</li>
-          <li>Versatile Wardrobe Staple</li>
-          <li>Available in Various Sizes</li>
-          <li>Tailored Fit</li>
-        </ul>`;
-
 const ProductPage = () => {
   return (
     <div className='container p-5 mx-auto space-y-8'>
       <ProductBreadCrumb />
 
       <div className='grid gap-8 md:mx-16 md:grid-cols-2 md:gap-16'>
-        <ProductSlide images={images} />
+        <ProductSlide images={productDetails.images} />
         <ProductDetails {...productDetails} />
       </div>
 
-      <ProductTabs description={description} />
+      <ProductTabs description={productDetails.description} />
     </div>
   );
 };
