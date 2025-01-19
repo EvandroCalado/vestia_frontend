d991709d8d1389988167ed212dfd3dda702dd9ab
Evandro Calado <evandrocalado07@gmail.com>
2025-01-19 00:09:24 -0300

    feat: Integrate Tooltip component into ProductColors for enhanced color selection feedback

--- src/components/product/product-colors/product-colors.tsx
+++ src/components/product/product-colors/product-colors.tsx
@@ -1,5 +1,11 @@
 'use client';
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui';
 import { cn } from '@/lib/utils';
 import { useVariantStore } from '@/stores';
 
@@ -23,17 +29,25 @@ export const ProductColors = ({ variants }: ProductColorsProps) => {
       </h5>
       <div className='flex items-center gap-4'>
         {uniqueColors.map((uniqueColor) => (
-          <button
-            key={uniqueColor.title}
-            style={{ backgroundColor: uniqueColor.color }}
-            className={cn(
-              'w-6 h-6 duration-150 rounded-full ring-1 ring-transparent hover:ring-primary ring-offset-4',
-              {
-                'ring-primary': color === uniqueColor.title,
-              },
-            )}
-            onClick={() => setColor(uniqueColor.title)}
-          />
+          <TooltipProvider key={uniqueColor.title}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <button
+                  style={{ backgroundColor: uniqueColor.color }}
+                  className={cn(
+                    'w-6 h-6 duration-150 rounded-full ring-1 ring-transparent hover:ring-primary ring-offset-4',
+                    {
+                      'ring-primary': color === uniqueColor.title,
+                    },
+                  )}
+                  onClick={() => setColor(uniqueColor.title)}
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{uniqueColor.title}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         ))}
       </div>
     </div>
