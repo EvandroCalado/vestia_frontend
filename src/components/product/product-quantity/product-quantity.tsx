918deecd2236de02ddce3a08d0c8d803859d5c92
Evandro Calado <evandrocalado07@gmail.com>
2025-01-18 23:57:28 -0300

    feat: Refactor ProductQuantity component to utilize variant store for dynamic quantity management

--- src/components/product/product-quantity/product-quantity.tsx
+++ src/components/product/product-quantity/product-quantity.tsx
@@ -1,43 +1,36 @@
 'use client';
 
-import { useState } from 'react';
-
 import { Button } from '@/components/ui';
+import { useVariantStore } from '@/stores';
 import { MinusIcon, PlusIcon } from 'lucide-react';
 
+import { Variant } from '../product-details';
+
 type ProductQuantityProps = {
-  quantity: number;
+  variants: Variant[];
 };
 
-export const ProductQuantity = ({ quantity }: ProductQuantityProps) => {
-  const [currentQuantity, setCurrentQuantity] = useState(1);
+export const ProductQuantity = ({ variants }: ProductQuantityProps) => {
+  const { color, size, quantity, incQuantity, decQuantity } = useVariantStore();
 
-  const checkQuantity = (qtd: number) => {
-    if (qtd < 1) setCurrentQuantity(1);
-
-    if (qtd > quantity) setCurrentQuantity(quantity);
-
-    return qtd;
-  };
+  const currentQuantity = variants
+    .filter((variant) => variant.title === color && variant.size === size)
+    .map((variant) => variant.quantity)[0];
 
   return (
     <div className='space-y-4 text-start'>
       <h5 className='font-normal uppercase text-muted-foreground'>quantity</h5>
       <div className='flex items-center border-[1px] rounded-md border-gray-200 w-max'>
-        <Button
-          variant='ghost'
-          className='w-9 h-9'
-          onClick={() => setCurrentQuantity(currentQuantity - 1)}
-        >
+        <Button variant='ghost' className='w-9 h-9' onClick={decQuantity}>
           <MinusIcon />
         </Button>
         <span className='flex items-center justify-center w-9 h-9'>
-          {checkQuantity(currentQuantity)}
+          {quantity}
         </span>
         <Button
           variant='ghost'
           className='w-9 h-9'
-          onClick={() => setCurrentQuantity(currentQuantity + 1)}
+          onClick={() => incQuantity(currentQuantity)}
         >
           <PlusIcon />
         </Button>
