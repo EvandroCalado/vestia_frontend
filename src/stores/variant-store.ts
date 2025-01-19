c4f440a4233c4fb192c76023c4fc25b5fdfca4a1
Evandro Calado <evandrocalado07@gmail.com>
2025-01-18 22:21:16 -0300

    feat: Add variant store for managing product variants and quantities

---
+++ src/stores/variant-store.ts
@@ -0,0 +1,37 @@
+import { create } from 'zustand';
+
+type VariantStore = {
+  color: string;
+  size: string;
+  quantity: number;
+  setColor: (variant: string) => void;
+  setSize: (variant: string) => void;
+  incQuantity: (maxInStock: number) => void;
+  decQuantity: () => void;
+};
+
+export const useVariantStore = create<VariantStore>()((set, get) => ({
+  color: '',
+  size: '',
+  quantity: 1,
+
+  setColor: (variant) => set({ color: variant }),
+
+  setSize: (variant) => set({ size: variant }),
+
+  incQuantity: (maxInStock = 1) => {
+    const { quantity } = get();
+
+    if (quantity >= maxInStock) return set({ quantity: maxInStock });
+
+    set({ quantity: quantity + 1 });
+  },
+
+  decQuantity: () => {
+    const { quantity } = get();
+
+    if (quantity <= 1) return set({ quantity: 1 });
+
+    set({ quantity: quantity - 1 });
+  },
+}));
