8c7b62d3d8ec8291bc757eb772ecec43fe7ba7f2
Evandro Calado <evandrocalado07@gmail.com>
2025-01-18 23:58:53 -0300

    feat: Implement dynamic product fetching and notFound handling in ProductPage component

--- src/app/(app)/products/[slug]/page.tsx
+++ src/app/(app)/products/[slug]/page.tsx
@@ -1,66 +1,31 @@
+import { getProductBySlugAction } from '@/actions';
 import {
   ProductBreadCrumb,
   ProductDetails,
   ProductSlide,
-  ProductTabs,
 } from '@/components/product';
+import { notFound } from 'next/navigation';
 
-const productDetails = {
-  title: 'raw black t-shirt lineup',
-  description: `<p>
-          Elevate your everyday style with our Men's Black T-Shirts, the
-          ultimate wardrobe essential for modern men. Crafted with meticulous
-          attention to detail and designed for comfort, these versatile black
-          tees are a must-have addition to your collection. The classic black
-          color never goes out of style. Whether you're dressing up for a
-          special occasion or keeping it casual, these black t-shirts are the
-          perfect choice, effortlessly complementing any outfit.
-        </p>
-        <ul>
-          <li>Premium Quality</li>
-          <li>Versatile Wardrobe Staple</li>
-          <li>Available in Various Sizes</li>
-          <li>Tailored Fit</li>
-        </ul>`,
-  score: 4.2,
-  numReviews: 54,
-  inStock: true,
-  price: 75,
-  images: [
-    '/images/slide1.png',
-    '/images/slide2.png',
-    '/images/slide3.png',
-    '/images/slide4.png',
-  ],
-  colors: [
-    {
-      name: 'blue',
-      color: '#9fbcf4',
-    },
-    {
-      name: 'yellow',
-      color: '#ffd792',
-    },
-    {
-      name: 'green',
-      color: '#7eb28e',
-    },
-  ],
-  sizes: ['s', 'l', 'xl'],
-  quantity: 5,
+type ProductPageProps = {
+  params: Promise<{ slug: string }>;
 };
 
-const ProductPage = () => {
+const ProductPage = async ({ params }: ProductPageProps) => {
+  const { slug } = await params;
+  const product = await getProductBySlugAction(slug);
+
+  if (!product) notFound();
+
   return (
     <div className='container p-5 mx-auto space-y-8'>
       <ProductBreadCrumb />
 
       <div className='grid gap-8 md:mx-16 md:grid-cols-2 md:gap-16'>
-        <ProductSlide images={productDetails.images} />
-        <ProductDetails {...productDetails} />
+        <ProductSlide images={product?.variants[0].images} />
+        <ProductDetails {...product} />
       </div>
 
-      <ProductTabs description={productDetails.description} />
+      {/* <ProductTabs description={product?.description} /> */}
     </div>
   );
 };
