3d5fec0f4e0e3e8b200bf417d3c9cbf4c13e8105
Evandro Calado <evandrocalado07@gmail.com>
2025-01-18 23:57:22 -0300

    feat: Refactor ProductSizes component to use variants for dynamic size selection

--- src/components/product/product-sizes/product-sizes.tsx
+++ src/components/product/product-sizes/product-sizes.tsx
@@ -1,31 +1,46 @@
+'use client';
+
 import { cn } from '@/lib/utils';
+import { useVariantStore } from '@/stores';
+
+import { Variant } from '../product-details';
 
 const allSizes = ['s', 'm', 'x', 'xl', 'xxl'];
 
 type ProductSizesProps = {
-  sizes: string[];
+  variants: Variant[];
 };
 
-export const ProductSizes = ({ sizes }: ProductSizesProps) => {
+export const ProductSizes = ({ variants }: ProductSizesProps) => {
+  const { color, size, setSize } = useVariantStore();
+
+  const currentSizes = variants
+    .filter((variant) => variant.title === color)
+    .map((variant) => variant.size);
+
   return (
     <div className='space-y-4'>
       <h5 className='font-normal uppercase text-muted-foreground'>
         select size
       </h5>
       <div className='flex items-center gap-4'>
-        {allSizes.map((size) => (
+        {allSizes.map((singleSize) => (
           <button
-            key={size}
-            disabled={!sizes.includes(size)}
+            key={singleSize}
+            disabled={!currentSizes.includes(singleSize)}
             className={cn(
               'w-9 h-9 uppercase border-[1px] rounded-md border-gray-200 duration-150 hover:border-primary',
               {
                 'disabled:opacity-30 hover:border-gray-200':
-                  !sizes.includes(size),
+                  !currentSizes.includes(singleSize),
+              },
+              {
+                'border-primary': singleSize === size,
               },
             )}
+            onClick={() => setSize(singleSize)}
           >
-            {size}
+            {singleSize}
           </button>
         ))}
       </div>
