25c35b13bac21b28a00841f68d091722b65b56fd
Evandro Calado <evandrocalado07@gmail.com>
2025-01-27 10:14:24 -0300

    feat: enhance ProductAddToCart component to utilize variants and improve cart functionality

--- src/components/product/product-add-to-cart/product-add-to-cart.tsx
+++ src/components/product/product-add-to-cart/product-add-to-cart.tsx
@@ -3,18 +3,37 @@
 import { useEffect } from 'react';
 
 import { Button } from '@/components/ui';
-import { useValidationMessage } from '@/hooks';
+import { useValidationMessage, useVariants } from '@/hooks';
 import { HeartIcon } from '@/icons';
-import { useVariantStore } from '@/stores';
+import { useCartStore, useVariantStore } from '@/stores';
+import { Variant } from '../product-details';
 
-export const ProductAddToCart = () => {
-  const { color, size, reset } = useVariantStore();
+type ProductAddToCartProps = {
+  variants: Variant[];
+};
+
+export const ProductAddToCart = ({ variants }: ProductAddToCartProps) => {
+  const { color, size, quantity, reset } = useVariantStore();
+  const { addProductToCart, cart } = useCartStore();
+  const { currentVariant } = useVariants(variants);
   const colorValidation = useValidationMessage(color, 'Please select a color.');
   const sizeValidation = useValidationMessage(size, 'Please select a size.');
+
+  console.log(cart);
 
   const handleAddToCart = () => {
     colorValidation.validate();
     sizeValidation.validate();
+
+    addProductToCart({
+      id: `${currentVariant.color}-${currentVariant.size}-${currentVariant.quantity}`,
+      title: currentVariant.title,
+      image: currentVariant.images[0],
+      color,
+      size,
+      price: currentVariant.price,
+      quantity,
+    });
   };
 
   useEffect(() => {
@@ -25,7 +44,7 @@ export const ProductAddToCart = () => {
 
   return (
     <div className='flex flex-col items-start space-y-4'>
-      <div className='flex items-center w-full gap-4'>
+      <div className='flex w-full items-center gap-4'>
         <Button className='w-full md:w-72' onClick={handleAddToCart}>
           Add to cart
         </Button>
@@ -36,15 +55,15 @@ export const ProductAddToCart = () => {
 
       <div className='flex items-center gap-4'>
         {colorValidation.showMessage && (
-          <p className='text-destructive text-xs'>{colorValidation.message}</p>
+          <p className='text-xs text-destructive'>{colorValidation.message}</p>
         )}
         {sizeValidation.showMessage && (
-          <p className='text-destructive text-xs'>{sizeValidation.message}</p>
+          <p className='text-xs text-destructive'>{sizeValidation.message}</p>
         )}
       </div>
 
       <h6 className='flex items-center justify-center gap-2 uppercase text-muted-foreground'>
-        <span className='bg-muted-foreground w-4 h-0.5 inline-block' />
+        <span className='inline-block h-0.5 w-4 bg-muted-foreground' />
         free shipping on orders $100+
       </h6>
     </div>
