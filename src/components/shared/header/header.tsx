9d6a2ae1d7582eeb3607b9718ca3f8c278ae3dc7
Evandro Calado <evandrocalado07@gmail.com>
2025-01-28 09:54:30 -0300

    refactor: remove hardcoded cart items from Header component

--- src/components/shared/header/header.tsx
+++ src/components/shared/header/header.tsx
@@ -6,27 +6,6 @@ import { PopUpHeader } from '../pop-up-header';
 import { ProfileMenu } from '../profile-menu';
 import { Search } from '../search';
 
-const items = [
-  {
-    id: 1,
-    title: 'raw black t-shirt lineup',
-    image: '/images/product1.png',
-    color: 'black',
-    size: 'm',
-    price: 75,
-    quantity: 1,
-  },
-  {
-    id: 2,
-    title: 'essential neutrals',
-    image: '/images/product2.png',
-    color: 'white',
-    size: 's',
-    price: 22,
-    quantity: 1,
-  },
-];
-
 export const Header = () => {
   return (
     <>
@@ -37,7 +16,7 @@ export const Header = () => {
         <Search className='max-md:hidden' />
 
         <div className='flex items-center gap-4'>
-          <CartMenu items={items} />
+          <CartMenu />
           <ProfileMenu />
           <Menu />
         </div>
