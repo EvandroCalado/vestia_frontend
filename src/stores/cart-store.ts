d9c0f5a76b36b9f361feaa707e5c84d766327abc
Evandro Calado <evandrocalado07@gmail.com>
2025-01-28 09:53:30 -0300

    feat: enhance cart store with persistence and summary calculations

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,14 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-type CartItem = {
+type SummaryInfo = {
+  subTotal: number;
+  tax: number;
+  total: number;
+  totalProductsInCart: number;
+};
+
+export type CartItem = {
   id: string;
   title: string;
   image: string;
@@ -12,34 +20,91 @@ type CartItem = {
 
 type CartStore = {
   cart: CartItem[];
+  getTotalProductsInCart: () => number;
+  getSummaryInfo: () => SummaryInfo;
   addProductToCart: (cartItem: CartItem) => void;
+  updateProductQuantity: (cartItem: CartItem, quantity: number) => void;
+  removeProductFromCart: (cartItem: CartItem) => void;
 };
 
-export const useCartStore = create<CartStore>()((set, get) => ({
-  cart: [],
+export const useCartStore = create<CartStore>()(
+  persist(
+    (set, get) => ({
+      cart: [],
 
-  addProductToCart: (cartItem) => {
-    const { cart } = get();
+      getTotalProductsInCart: () => {
+        const { cart } = get();
 
-    const productInCart = cart.some(
-      (item) => item.id === cartItem.id && item.size === cartItem.size,
-    );
+        return cart.reduce((acc, item) => acc + item.quantity, 0);
+      },
 
-    if (!productInCart) {
-      set({ cart: [...cart, cartItem] });
-      return;
-    }
+      getSummaryInfo: () => {
+        const { cart, getTotalProductsInCart } = get();
 
-    const newCart = cart.map((item) => {
-      if (item.id === cartItem.id && item.size === cartItem.size) {
-        return {
-          ...item,
-          quantity: item.quantity + cartItem.quantity,
-        };
-      }
-      return item;
-    });
+        const subTotal = cart.reduce(
+          (acc, item) => acc + item.price * item.quantity,
+          0,
+        );
 
-    set({ cart: newCart });
-  },
-}));
+        const tax = subTotal * 0.15;
+        const total = subTotal + tax;
+        const totalProductsInCart = getTotalProductsInCart();
+
+        return { subTotal, tax, total, totalProductsInCart };
+      },
+
+      addProductToCart: (cartItem) => {
+        const { cart } = get();
+
+        const productInCart = cart.some(
+          (item) => item.id === cartItem.id && item.size === cartItem.size,
+        );
+
+        if (!productInCart) {
+          set({ cart: [...cart, cartItem] });
+          return;
+        }
+
+        const newCart = cart.map((item) => {
+          if (item.id === cartItem.id && item.size === cartItem.size) {
+            return {
+              ...item,
+              quantity: item.quantity + cartItem.quantity,
+            };
+          }
+          return item;
+        });
+
+        set({ cart: newCart });
+      },
+
+      updateProductQuantity: (cartItem, quantity) => {
+        const { cart } = get();
+
+        const newCart = cart.map((item) => {
+          if (item.id === cartItem.id && item.size === cartItem.size) {
+            return {
+              ...item,
+              quantity,
+            };
+          }
+
+          return cartItem;
+        });
+
+        set({ cart: newCart });
+      },
+
+      removeProductFromCart: (cartItem) => {
+        const { cart } = get();
+
+        const newCart = cart.filter(
+          (item) => !(item.id === cartItem.id && item.size === cartItem.size),
+        );
+
+        set({ cart: newCart });
+      },
+    }),
+    { name: 'vestia-cart-storage' },
+  ),
+);
