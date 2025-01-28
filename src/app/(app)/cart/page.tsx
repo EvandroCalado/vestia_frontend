073ce027cb7248f8b5a50c7f48893fbd40961959
Evandro Calado <evandrocalado07@gmail.com>
2025-01-28 09:56:29 -0300

    refactor: simplify CartPage component by removing hardcoded items and summary

--- src/app/(app)/cart/page.tsx
+++ src/app/(app)/cart/page.tsx
@@ -1,45 +1,18 @@
 import { CartItems, CartSummary } from '@/components/cart';
 import { Breadcrumb } from '@/components/shared';
 
-const items = [
-  {
-    id: 1,
-    title: 'raw black t-shirt lineup',
-    image: '/images/product1.png',
-    color: 'black',
-    size: 'm',
-    price: 75,
-    quantity: 1,
-  },
-  {
-    id: 2,
-    title: 'essential neutrals',
-    image: '/images/product2.png',
-    color: 'white',
-    size: 's',
-    price: 22,
-    quantity: 1,
-  },
-];
-
-const summary = {
-  subtotal: 90,
-  tax: 3,
-  total: 100,
-};
-
 const CartPage = () => {
   return (
-    <div className='container mx-auto p-5'>
+    <div className='container relative mx-auto h-full p-5'>
       <Breadcrumb />
 
-      <div className='grid py-16 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid pt-10 md:grid-cols-2 lg:grid-cols-3'>
         <div className='lg:col-span-2'>
           <h4>You cart</h4>
-          <CartItems items={items} />
+          <CartItems />
         </div>
 
-        <CartSummary {...summary} />
+        <CartSummary />
       </div>
     </div>
   );
