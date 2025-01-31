00906169173fdd5a173a8b56729d3c3bf65d89d4
Evandro Calado <evandrocalado07@gmail.com>
2025-01-31 10:23:38 -0300

    feat: add Select component for enhanced UI selection functionality

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -7,6 +7,7 @@ export * from './carousel';
 export * from './checkbox';
 export * from './input';
 export * from './label';
+export * from './select';
 export * from './separator';
 export * from './sheet';
 export * from './slider';
