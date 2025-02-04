a83cdb8ef6f3390a418c3e5bd268429d678cdae8
Evandro Calado <evandrocalado07@gmail.com>
2025-02-04 09:39:53 -0300

    feat: enhance getAllProductsAction to support filtering by categories, color, size, and price

--- src/actions/get-all-products-action.ts
+++ src/actions/get-all-products-action.ts
@@ -7,10 +7,29 @@ import { StrapiAllProducts } from '@/types';
 type SearchParams = { [key: string]: string | string[] | undefined };
 
 export const getAllProductsAction = async (searchParams: SearchParams) => {
-  console.log(searchParams);
+  const paramCategories = Array.isArray(searchParams.categories)
+    ? searchParams.categories
+    : [searchParams.categories].filter(Boolean);
+
+  const queryCategories = paramCategories
+    .map((category) => `&filters[categories][$eq]=${category}`)
+    .join('');
+
+  const queryColor = searchParams.color
+    ? `&filters[variant][title][$eq]=${searchParams.color}`
+    : '';
+
+  const querySize = searchParams.size
+    ? `&filters[variant][size][$eq]=${searchParams.size}`
+    : '';
+
+  const queryPrice =
+    searchParams.price && Number(searchParams.price) > 0
+      ? `&filters[variant][price][$lte]=${searchParams.price}`
+      : '';
 
   const url = new URL(
-    `/api/products?populate[variant][populate][0]=images`,
+    `/api/products?populate[variant][populate][0]=images${queryCategories}${queryColor}${querySize}${queryPrice}`,
     STRAPI_URL,
   );
 
